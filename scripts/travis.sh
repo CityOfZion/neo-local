d6c3b3f29f34f6b5efa8429ef446bc0e19bc7664
Jeroen Peeters <contact@jeroenpeeters.me>
2018-08-20 16:11:27 +0200

    Fix tagged builds (#40)
    * chore: remove spaces in docker-compose.yml
    
    * chore: get version info
    
    * chore: cat info from correct file

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Script that Travis CI will run to test the project, it is conditional on the branch.
 
+version=$(cat ./VERSION)
+
 set -e
 
-if [[ $TRAVIS_BRANCH == 'master' ]] 
+if [[ $TRAVIS_BRANCH == 'master' || $TRAVIS_TAG == $version ]]
 then
   make integration-tests
 else
