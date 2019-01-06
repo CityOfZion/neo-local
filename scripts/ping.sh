9edfc3ab2bd8b427c6aee901ca5eda3b035dc4c0
CryptoTuga <nuno@justoweb.com>
2019-01-06 19:34:41 +0000

    adding healthcheck for neo-faucet (#141)
    Merging

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 # Script to check and wait until the NeoScan service is operational. 
 
+while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-local_autoheal)" != "\"healthy\"" ]
+  do
+    printf '.'
+    sleep 3
+  done
+
 while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-scan-api)" != "\"healthy\"" ]
   do
     printf '.'
     sleep 3
   done
 
-while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-local_autoheal)" != "\"healthy\"" ]
+while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-faucet)" != "\"healthy\"" ]
   do
     printf '.'
     sleep 3
